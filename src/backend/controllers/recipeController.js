d6f09813ba39436e8f1020064699a6b32a3bfe80
Daniel Vršek <daniel.vrsek@gmail.com>
2021-06-13 15:41:34 +0200

    Update recipeController.js

--- src/backend/controllers/recipeController.js
+++ src/backend/controllers/recipeController.js
@@ -87,7 +87,7 @@ class RecipeController extends Controller {
         await unitOfWork.commitAsync();
     }
 
-    async deleteRecipeAsync(recipeId) {
+    async deleteRecipeAsync(recipeId, context) {
         let username = context.authorization.username;
         let author = this.getAuthor(username);
 
